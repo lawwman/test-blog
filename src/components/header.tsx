25327341229d77e2d616ec2495d5df81e4df2b35
lawwman <lawrence_koh@tech.gov.sg>
2023-04-27 17:15:22 +0800

    test local storage

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,16 @@
 
 import Link from "next/link";
+import { useRouter } from 'next/router';
 
 function Header() {
+    const router = useRouter();
   return (
     <nav>
-      <Link href="/">
-      Home
+      <Link href="https://elegant-dolphin-e14032.netlify.app/">
+        Home
       </Link>
-      <Link href="/blog">
-      Blog
+      <Link href="/">
+        Blog
       </Link>
     </nav>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import styles from "../styles/Home.module.css";
 import Header from "../components/header";
+import { useEffect } from "react";
 
 export default function Home() {
+  /* on start up */
+  useEffect(() => {
+    console.log('blog accessing local storage...')
+    console.log(window.localStorage.getItem('testStuff'));
+}, []);
   return (
     <div className={styles.container}>
       <Header />
